3a35e5315117d887df6fadd9ef5384cba14181a3
Elip19 <116365345+Elip19@users.noreply.github.com>
2024-12-23 11:05:15 +0100

    modificato la supply chain

--- src/SupplyChain.java
+++ src/SupplyChain.java
@@ -1,39 +1,32 @@
  import java.util.ArrayList;
 import java.util.List;
 
-public class SupplyChain extends Chain {
-    //private Chain chain;
+public class SupplyChain {
+    private Chain chain;
     private List<String> prodotto;
 
-    public SupplyChain(List<Block>chain, List<String> prodotto) {
-        super ();
-        prodotto = new ArrayList<>(prodotto);
+    public SupplyChain() {
+        this.chain = new Chain();
+        this.prodotto = new ArrayList<>();
 
 
     }
-//aggiungi prodotto alla supplyChain
+
+    //aggiungi prodotto alla supplyChain
     public boolean addProdotto(String prodotto) {
         return this.prodotto.add(prodotto);
     }
-    // aggiungi transazione nella catena
-    public void addTransaction(String prodotto,String mittente,String destinatario,String descrizione) {
-        Transaction newTransaction = new Transaction(mittente, destinatario, descrizione);
 
-        //aggiungo nella lista delle transazioni
-        List <Transaction> transactions= new ArrayList<>();
-        transactions.add(newTransaction);
-        //quando creo una nuova transazione aggiungo un blocco alla catena
-        Block previousBlock =chain.get(chain.size()-1);
-        Block newBlock= new Block(
-                previousBlock.getId()+1, //incremento indice
-                previousBlock.getHashCorrente(),//uso l'hash del blocco precedente
-                transactions //Transazione del nuovo blocco
-        );
-        chain.add(newBlock);
+    public List<String> getProdotto() {
+        return this.prodotto;
     }
-    //visualizzo la Blockchain
-   public List visualizzoBlock() {
-        return new ArrayList<>(chain); //restituisce la lista dei blocchi nella Blockchain
-   }
+    // aggiungi transazione nella catena
+    public void addTransaction(List<Transaction> transactions) {
+        chain.addBlock(transactions);
+    }
+    //restituisce la rappresentazione della blockchain
+    public String getChain() {
+        return chain.toString();
+    }
 
 }
